2645293385d3225f522a6f8d904901fcbe1c8925
LevchenkoLudmila <104581677+LevchenkoLudmila@users.noreply.github.com>
2022-07-28 00:49:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-const input1 = document.querySelector ('.kat1');
-console.log (input1);
-input1.addEventListener('chenge', () =>{
-    const a = Number(document.querySelector('.kat1').value);
-    document.querySelector('.gipotenuza1').innerHTML = a;
-});
-
-const input2 = document.querySelector('.kat2');
-console.log (input2);
-input2.addEventListener('change', () => {
-   const b = Number(document.querySelector('.kat2').value);
-   document.querySelector('.gipotenuza1').innerHTML = b;
-});
+const butnRezult = document.querySelector('.btn');
 const gipotenuzaDiv = document.querySelector('.gipotenuza1');
 console.log (gipotenuzaDiv);
-gipotenuzaDiv.addEventListener('chenge', () => {
-    const result = Math.sqrt(a*a + b*b);
-    return result;
+console.log(butnRezult);
+butnRezult.addEventListener('click', () =>{
+    let a = Number(document.querySelector ('.katet1').value);
+    let b = Number(document.querySelector ('.katet2').value);
+    const gip = Math.sqrt((a*a) + (b*b));
+    document.querySelector('.gipotenuza1').innerHTML = gip;
 });
 
